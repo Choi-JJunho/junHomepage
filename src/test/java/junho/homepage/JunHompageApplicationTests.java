d4144d7dcd75858aace1946abb2cad277f7a8d0b
Choi-JJunho <junho5336@gmail.com>
2022-06-21 14:47:04 +0900

    init commit

---
+++ src/test/java/junho/homepage/JunHompageApplicationTests.java
@@ -0,0 +1,13 @@
+package junho.homepage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunHompageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
