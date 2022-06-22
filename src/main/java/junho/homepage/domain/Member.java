f53d1c6a6806f05ec8e5b5079f0e0da53c8bffdc
Choi-JJunho <junho5336@gmail.com>
2022-06-22 16:26:43 +0900

    Add business logic
    Add Member business logic

--- src/main/java/junho/homepage/domain/Member.java
+++ src/main/java/junho/homepage/domain/Member.java
@@ -1,11 +1,18 @@
 package junho.homepage.domain;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 
     @Id @GeneratedValue
@@ -15,7 +22,7 @@ public class Member {
     private String account;
     private String password;
     private String email;
-    private String nickname;
+    private String name;
 
     private LocalDateTime createdAt;
     private LocalDateTime deletedAt;
@@ -27,4 +34,22 @@ public class Member {
     @OneToMany
     @JoinColumn(name = "board_id")
     private List<Board> boards = new ArrayList<>();
+
+    public Member(String account, String password, String email, String name) {
+        this.account = account;
+        savePassword(password);
+        this.email = email;
+        this.name = name;
+    }
+
+    // 비즈니스 로직
+    public void savePassword(String password) {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        this.password = bCryptPasswordEncoder.encode(password);
+    }
+
+    public boolean matchPassword(String password) {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        return bCryptPasswordEncoder.matches(password, this.password);
+    }
 }
--- src/main/java/junho/homepage/service/MemberService.java
+++ src/main/java/junho/homepage/service/MemberService.java
@@ -1,2 +1,45 @@
-package junho.homepage.service;public class MemberService {
+package junho.homepage.service;
+
+import junho.homepage.domain.Member;
+import junho.homepage.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public void signin(Member member) {
+        if (member.getAccount() == null){
+            throw new NullPointerException("아이디값이 비어있습니다.");
+        } else if (memberRepository.findByaccount(member.getAccount()) != null) {
+            throw new DuplicateKeyException("동일한 아이디가 존재합니다.");
+        }
+        if (memberRepository.findByname(member.getName()) != null) {
+            throw new DuplicateKeyException("동일한 이름이 존재합니다.");
+        }
+        if (memberRepository.findByemail(member.getEmail()) != null) {
+            throw new DuplicateKeyException("동일한 이메일이 존재합니다.");
+        }
+
+        memberRepository.save(member);
+    }
+
+    @Transactional(readOnly = true) // 향후 토큰 발급하는 방식으로 리팩토링 예정
+    public Boolean login(String account, String password) {
+        Member member = memberRepository.findByaccount(account);
+        if(member == null) {
+            throw new NullPointerException("존재하지 않는 아이디입니다");
+        } else {
+            return member.matchPassword(password);
+        }
+    }
+
+
+
 }
